afb258bca152479a5c3ed4e3ffdb716c6ff79aec
Eric J Duran <39136400+ericduran1@users.noreply.github.com>
2018-05-30 19:00:25 -0400

    Add files via upload

---
+++ inc/data.php
@@ -0,0 +1,326 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+    "title" => "Fahrenheit 451",
+    "img" => "img/media/book1.png",
+   "genre" => "Utopian and dystopian fiction",
+   "format" => "Paperback",
+   "year" => 1953,
+   "category" => "Books",
+   "authors" => [
+       "Ray Bradbury",
+   ],
+   "publisher" => "Ballantine Books",
+   "isbn" => '9781451673319'
+];
+
+$catalog[102] = [
+    "title" => "The Kite Runner",
+    "img" => "img/media/book2.png",
+   "genre" => "Historical Fiction",
+   "format" => "Paperback",
+   "year" => 2013,
+   "category" => "Books",
+   "authors" => [
+       "Khaled Hosseini"
+   ],
+   "publisher" => "Penguin Publishing Group",
+   "isbn" => '9781594631931'
+];
+
+$catalog[103] = [
+    "title" => "The Crossing",
+    "img" => "img/media/book3.png",
+   "genre" => "Fiction",
+   "format" => "Paperback",
+   "year" => 1987,
+   "category" => "Books",
+   "authors" => [
+       "Gary Paulsen"
+   ],
+   "publisher" => "Penguin Publishing Group",
+   "isbn" => '9780833545817'
+];
+
+$catalog[104] = [
+    "title" => "The Alchemist",
+    "img" => "img/media/book4.png",
+   "genre" => "Quest",
+              "Adventure Fiction",
+              "Fantasy Fiction",
+   "format" => "Paperback",
+   "year" => 2014,
+   "category" => "Books",
+   "authors" => [
+       "Paulo Coelho",
+   ],
+   "publisher" => "HarperCollins Publishers",
+   "isbn" => '9780062355300'
+];
+
+//Movies
+$catalog[201] = [
+   "title" => "Avengers Infinity War",
+   "img" => "img/media/movie1.png",
+   "genre" => "Action Film,Adventure Film,Fantasy,Science Fiction Film,Superhero Movie",
+   "format" => "Printed Film",
+   "year" => 2018,
+   "category" => "Movies",
+   "director" => "Anthony Russo,Joe Russo",
+   "writers" => [
+       "Stephen McFeely",
+       "Christopher Markus",
+
+   ],
+   "stars" => [
+       "Robert Downey Jr.",
+       "Chris Pratt",
+       "Josh Brolin",
+       "Chris Evans",
+       "Chris Hemsworth",
+       "Scarlett Johansson",
+       "Tom Holland",
+       "Elizabeth Olsen",
+       "Benedict Cumberbatch",
+       "Karen Gillan",
+       "Zoe Saldana",
+       "Mark Ruffalo",
+       "Vin Diesel",
+       "Dave Bautista",
+       "Paul Bettany",
+       "Sebastian Stan",
+       "Benedict Wong",
+       "Pom Klementieff",
+       "Stan Lee",
+       "Tom Hiddleston",
+       "Danai Gurira",
+       "Benicio Del Toro",
+       "Anthony Mackie",
+       "Peter Dinklage",
+       "Terry Notary",
+       "Chadwick Boseman",
+       "Idris Elba",
+       "Bradley Cooper",
+       "Isabella Amara",
+       "Tom Vaughan-lawlor",
+       "Letitia Wright",
+       "Carrie Coon",
+       "Gwyneth Paltrow",
+       "Don Cheadle"
+
+   ]
+];
+
+$catalog[202] = [
+   "title" => "The Cabin In The Woods",
+   "img" => "img/media/movie2.png",
+   "genre" => "Thriller,Mystery",
+   "format" => "Blue-ray, Digital Copy",
+   "year" => 2011,
+   "category" => "Movies",
+   "director" => "Drew Goddard",
+   "writers" => [
+       "Joss Whedon",
+       "Drew Goddard"
+
+   ],
+   "stars" => [
+       "Chris Hemsworth",
+       "Kristen Connolly",
+       "Anna Hutchison",
+       "Jesse Williams",
+       "Fran Kranz",
+       "Richard Jenkins",
+       "Bradley Whitford",
+       "Amy Acker",
+       "Brian J. White",
+       "Sigourney Weaver",
+       "Jodelle Ferland",
+       "Tom Lenk",
+       "Tim De Zarn",
+       "Dan Payne",
+       "Maya Massar",
+       "Dan Shea",
+       "Chelah Horsdal",
+       "Richard Cetrone",
+       "Monique Ganderton"
+
+   ]
+];
+
+$catalog[203] = [
+   "title" => "Star Wars: The Last Jedi",
+   "img" => "img/media/movie3.png",
+   "genre" => "Action Film,Adventure Film,Science Fiction Film,Fantasy",
+   "format" => "Blue-ray,DVD,Digital",
+   "year" => 2017,
+   "category" => "Movies",
+   "director" => "Rian Johnson",
+   "writers" => [
+       "Rian Johnson"
+
+   ],
+   "stars" => [
+       "Mark Hamill",
+       "Carrie Fisher",
+       "Adam Driver",
+       "Daisy Ridley",
+       "John Boyega",
+       "Oscar Isaac",
+       "Andy Serkis",
+       "Lupita Nyong'o",
+       "Domhnall Gleeson",
+       "Anthony Daniels",
+       "Gwendoline Christie",
+       "Kelly Marie Tran",
+       "Laura Dern",
+       "Benicio Del Toro",
+       "Frank Oz",
+       "Billie Lourd",
+       "Joonas Suotamo",
+       "Amanda Lawrence",
+       "Jimmy Vee",
+       "Brian Herring",
+       "Dave Chapman",
+       "Justin Theroux",
+       "Tim Rose",
+       "Tom Kane",
+       "Adrian Edmondson",
+       "Mark Lewis Jones",
+       "Hermione Corfield",
+       "Veronica Ngo",
+       "Noah Segan",
+       "Jamie Christopher",
+       "Paul Kasey",
+       "Michaela Coel",
+       "Jonathan Harden",
+       "Dan Euston",
+       "Priyanga Burford",
+       "Navin Chowdhry",
+       "Andrew Jack",
+       "Crystal Clarke",
+       "Aki Omoshaybi",
+       "Togo Igawa",
+       "Hugh Skinner",
+       "Tim Steed",
+       "Simon Lowe",
+       "Joe Van Moyland",
+       "Shauna Macdonald",
+       "Darren Morfitt",
+       "Gerard Monaco",
+       "Kate Dickie",
+       "Patrick O'Kane",
+       "Paul Bazely",
+       "Orion Lee",
+       "Amira Ghazalla",
+       "Ralph Ineson",
+       "Akshay Kumar",
+       "Michael Jibson",
+       "Luke Neal",
+       "Andy Nyman",
+       "Temirlan Blaev",
+       "Josiah Oniha",
+       "Sara Heller",
+       "Matthew Sharp",
+       "Lily Cole",
+       "Warwick Davis",
+       "Joseph Gordon-Levitt",
+       "Kiran Shah",
+       "Mike Quinn",
+       "Gareth Edwards",
+       "Jack Greenlees",
+       "Danny Sapani",
+       "Kevin Layne",
+       "Ben Morris"
+
+   ]
+];
+
+$catalog[204] = [
+   "title" => "Kill Bill: Volume 2",
+   "img" => "img/media/movie4.png",
+   "genre" => "Drama,Crime Film",
+   "format" => "DVD",
+   "year" => 2004,
+   "category" => "Movies",
+   "director" => "Quentin Tarantino",
+   "writers" => [
+       "Quentin Tarantino"
+
+   ],
+   "stars" => [
+       "Uma Thurman",
+       "David Carradine",
+       "Michael Madsen",
+       "Samuel L. Jackson",
+       "Lucy Liu",
+       "Chia Hui Liu",
+       "Michael Parks",
+       "Vivica A. Fox",
+       "Perla Haney-Jardine",
+       "Julie Dreyfus",
+       "Chiaki Kuriyama",
+       "Sonny Chiba",
+       "Bo Svenson",
+       "Christopher Allen Nelson",
+       "Michael Bowen",
+       "Caitlin Keats",
+       "Lasrry Bishop",
+       "Jeannie Epper",
+       "Jun Kunimura",
+       "Sid Haig",
+       "Laura Cayouette",
+       "James Parks",
+       "Ambrosia Kelley",
+       "Helen Kim"
+   ]
+];
+
+
+
+
+//Music
+$catalog[301] = [
+   "title" => "Bobby Tarantino 2",
+   "img" => "img/media/music1.png",
+   "genre" => "Hip-Hop",
+   "format" => "Digital",
+   "year" => 2018,
+   "category" => "Music",
+   "artist" => "Logic"
+];
+
+
+$catalog[302] = [
+   "title" => "Everybody",
+   "img" => "img/media/music2.png",
+   "genre" => "Conscious Hip-Hop",
+   "format" => "Digital",
+   "year" => 2017,
+   "category" => "Music",
+   "artist" => "Logic"
+];
+
+
+$catalog[303] = [
+   "title" => "KOD",
+   "img" => "img/media/music3.png",
+   "genre" => "Hip-Hop",
+   "format" => "Digital",
+   "year" => 2018,
+   "category" => "Music",
+   "artist" => "J. Cole"
+];
+
+
+$catalog[304] = [
+   "title" => "...And Justice For All",
+   "img" => "img/media/music4.png",
+   "genre" => "Heavy Metal,Thrash Metal",
+   "format" => "CD,Digital",
+   "year" => 1988,
+   "category" => "Music",
+   "artist" => "Metallica"
+];
+?>
---
+++ inc/footer.php
@@ -0,0 +1,12 @@
+<div class="footer">
+  <div class="wrapper">
+    <ul>
+      <li><a href="https://www.instagram.com/LaGuardiaCC/">twitter</a></li>
+      <li><a href="https://www.facebook.com/LaGuardiaCC">facebook</a></li>
+    </ul>
+    <p>&copy;<?php echo date("Y"); ?> My Online Library Collection</p>
+  </div>
+</div>
+
+</body>
+</html>
